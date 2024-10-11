976861055aee913fbcaa17ffbb87a9027aa08747
unknown <ooguuh@gmail.com>
2024-10-11 17:26:37 +0200

    adding dev args handler
    Signed-off-by: unknown <ooguuh@gmail.com>

--- utils.py
+++ utils.py
@@ -2,9 +2,21 @@ import sqlite3
 import requests
 from PIL import Image
 from io import BytesIO
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("dev", action="store_true", required=False, help="dev env")
+args = parser.parse_args()
+
+DEV_ENV = args.dev
+DATABASE_NAME = "bdd.sqlite"
+
+if DEV_ENV:
+    print("Development environment enabled")
+    DATABASE_NAME = "dev_bdd.sqlite"
 
 def connDb():
-    return sqlite3.connect("bdd.sqlite")
+    return sqlite3.connect(DATABASE_NAME)
 
 # merci chat gpt mdr 
 def calcul_avg_color(imageUrl):
